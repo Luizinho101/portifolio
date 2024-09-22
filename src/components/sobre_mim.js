0e66fdc39dcc208cc52de2fa1e1431912968331b
Luiz Antonio <luiz.antonio.da.silva777@gmail.com>
2024-09-22 00:58:29 -0300

    Adição de alteraçãoes na página sobre mim

--- src/components/sobre_mim.js
+++ src/components/sobre_mim.js
@@ -4,13 +4,12 @@ function Perfil() {
             <h1 className="text-center text-[20px] text-white mb-4 mt-16 col-start-2 col-span-10 text-base sm:text-base md:text-base lg:text-2xl xl:text-3xl">Sobre mim</h1>
             <div className="bg-gradient-to-r from-[#777CF4] via-[#BD79AF] to-[#F47777] text-white p-2 rounded-[40px] col-start-2 col-span-10">
                 <p className="lg:text-[20px] ml-4 mr-4 md:ml-16 md:ml-16 text-base sm:text-base md:text-base lg:text-2xl xl:text-3xl">
-                Olá! Sou o Luiz, um entusiasta do desenvolvimento de software com 24 anos de idade. Estou atualmente cursando 
-                Engenharia da Computação e estou determinado a me tornar um desenvolvedor back-end. Minha especialização 
-                principal está em desenvolvimento back-end com C#, mas também possuo habilidades sólidas em front-end, 
-                incluindo HTML, CSS e JavaScript. Desde 2021, tenho imergido no universo do C# e desenvolvimento de software, 
-                descobrindo uma verdadeira paixão pela linguagem e pela lógica por trás dela. Além disso, participei de um 
-                projeto de iniciação científica por 2 anos, desenvolvendo softwares em equipe e aprimorando minhas habilidades 
-                em programação e resolução de problemas.
+                    Olá! Sou Luiz, tenho 24 anos e sou entusiasta do desenvolvimento de software. Atualmente, estou cursando 
+                    Engenharia da Computação e me especializando em desenvolvimento back-end com foco principal em C#. Além 
+                    disso, possuo conhecimentos básicos em algumas linguagens front-end, como HTML, CSS e JavaScript. Desde 2021, 
+                    venho me dedicando ao estudo de C# e ao desenvolvimento de software, onde descobri uma verdadeira paixão pela 
+                    linguagem e pela lógica envolvida. Também participei por dois anos de um projeto de iniciação científica, 
+                    desenvolvendo software em equipe e aprimorando minhas habilidades em programação e resolução de problemas.
                 </p>
             </div>
         </div>
